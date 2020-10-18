6bf9c0dc767b4a79886ad78bba611be8b6b7464e
Rron Aliu <rronaliu55@gmail.com>
2020-10-19 00:24:18 +0200

    deploy

--- database.sql
+++ database.sql
@@ -3,14 +3,15 @@ CREATE DATABASE imageboard;
 CREATE TABLE post (
     post_id SERIAL PRIMARY KEY,
     description VARCHAR(255),
-    body VARCHAR(255),
-    created_at TIMESTAMP NOT NULL
+    body VARCHAR,
+    image VARCHAR(255),
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
-CREATE TABLE comment (
+CREATE TABLE comments (
     cid SERIAL PRIMARY KEY,
     comment VARCHAR,
     image VARCHAR(255),
     post_id INT REFERENCES post(post_id),
-    created_at TIMESTAMP NOT NULL
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
