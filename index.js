5260383d84c03e430fad66bda3471a61ada72478
Rron Aliu <rronaliu55@gmail.com>
2020-10-18 23:11:11 +0200

    put server code in root directory

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const pool = require("./db");
+const PORT = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
