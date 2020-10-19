ae76b1d68f7c33ee5db9135db79410a1c74f907c
Rron Aliu <rronaliu55@gmail.com>
2020-10-19 10:48:52 +0200

    more

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static("./client/build"));
+app.use(express.static(__dirname + "client/build"));
 
 // if(process.env.NODE_ENV === "production") {
 //   // server static content
