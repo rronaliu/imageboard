969a95969b74fc471fa497d2657115c53159be3a
Rron Aliu <rronaliu55@gmail.com>
2020-10-19 10:32:26 +0200

    commit

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ const PORT = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
-// app.use(express.static("./client/build"));
+app.use(express.static("./client/build"));
 
-if(process.env.NODE_ENV === "production") {
-  // server static content
-  //npm run build
-  app.use(express.static(path.join(__dirname, "client/build")));
-}
+// if(process.env.NODE_ENV === "production") {
+//   // server static content
+//   //npm run build
+//   app.use(express.static(path.join(__dirname, "client/build")));
+// }
 
 // ROUTES //
 
